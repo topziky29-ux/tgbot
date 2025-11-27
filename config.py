043d7b1307411ce0ff1c81a09b16a1c22ab02aca
topziky29-ux <topziky29@gmail.com>
2025-11-27 11:37:06 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+BOT_TOKEN = "8297386105:AAH3ombr86k2yJF3udsVnk_5Y46ZK1Y1DTc"
+MAIN_ADMIN_ID = 1246951810
+ADMIN_IDS = [MAIN_ADMIN_ID]
+GROUPS = ["ПСН-24", "ПСН-23", "ПСН-25", "ТСН-24", "ТСН-23", "СТН-25"]
+SUBGROUPS = ["1 п/гр", "2 п/гр", "Обе подгруппы"]
+BROADCAST_TIME = "19:00"
